f86891fafc0772bdc78cde7d8edd4f3c53dd2c2b
joaovict76 <joaovictoorree@gmail.com>
2024-09-17 22:01:06 -0300

    estudo de variaveis

---
+++ Fundamentos/variaveis.js
@@ -0,0 +1,31 @@
+/**
+    estudo de constante
+ *  @author Joao Victor
+*/
+
+console.clear()
+//constante 
+const pi =3.14
+
+//variáveis
+let nome
+let idade
+let vip
+
+nome = "Joao Victor"
+console.log(typeof(nome))
+console.log(nome)
+
+idade = "20"
+console.log(typeof(idade))
+console.log(idade)
+
+vip = true
+console.log(typeof(vip))
+console.log(vip)
+
+console.log(pi)
+//pi = 3.17
+
+nome = "Joao Victor" 
+console.log(nome)
