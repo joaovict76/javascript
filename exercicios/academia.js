5cb24eacf87c12e099a405df1e9e6218aa6edfb5
joaovict76 <joaovictoorree@gmail.com>
2024-09-19 22:18:29 -0300

    exercicios

---
+++ exercicios/academia.js
@@ -0,0 +1,29 @@
+/**
+ * Ficha de aluno de academia 
+ * @author João Victor
+ */
+
+//variaveis
+let nome 
+let idade
+let peso 
+let altura 
+let vip
+
+console.clear()
+
+// entrada de dados 
+nome = "joão Victor"
+idade = 20 
+peso = 82 
+altura = 1.76
+vip = true 
+
+//saida 
+console.log("Ficha do aluno")
+console.log("______________________")
+console.log(`nome: ${nome}`)
+console.log(`idade: ${idade}`)
+console.log(`peso: ${peso}`)
+console.log(`altura: ${altura}`)
+console.log(`vip: ${vip}`)
