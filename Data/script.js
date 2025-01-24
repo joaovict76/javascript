ee0814829c932862b79e9ea71c75bad429804259
JOAO VICTOR DA SILVA GUILHERME <joao.vsguilherme@senacsp.edu.br>
2025-01-23 22:20:36 -0300

    carrossel

---
+++ Data/script.js
@@ -0,0 +1,24 @@
+/**
+ * Data automatica no rodape 
+ * @author joão Victor
+ */
+
+function obterData() {
+    const data = new Date()
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    }
+    return `${data.toLocaleDateString('pt-BR', options)} - ${data.toLocaleDateString('pt-BR')}`
+}
+
+function atualizarData() {
+    //a linha abaixo insere na TAG <p> indentificando como dataatual e retorno de função
+    document.getElementById('dataAtual').innerHTML = obterData()
+
+}
+
+//atualizar a data a cada segundo
+setInterval(atualizarData,1000)//1000ms - 1s
